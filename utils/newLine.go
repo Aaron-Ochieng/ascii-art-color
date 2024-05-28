d34aa7f88e3f9788ccaed37e229152fc1112370c
aaochieng <ochiengaoron@gmail.com>
2024-05-28 11:02:55 +0300

    Handlenewline etc

--- utils/newLine.go
+++ utils/newLine.go
@@ -3,6 +3,7 @@ package color
 import "strings"
 
 func HandleNewLine(s string) ([]string, error) {
+	s = strings.ReplaceAll(s, "\\n", "\n")
 	spString := strings.Split(s, "\n")
 	inputParts, err := CheckIllegalChar(spString)
 	return inputParts, err
